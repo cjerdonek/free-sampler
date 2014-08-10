3b10f6c7a5bc43fa8a6dfdb456bd6e85626fb70c
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-08-09 23:09:48 -0700

    Enclose controller in anonymous function.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,18 +1,28 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name freeSamplerApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the freeSamplerApp
- */
-angular.module('freeSamplerApp', ['freeSamplerApp.services'])
-  .controller('MainCtrl', ['$scope', 'myHash', function ($scope, myHash) {
-    $scope.blah = myHash('FOO');
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }]);
+/* Controllers */
+
+(function(){
+
+    var samplerControllers = angular.module('freeSamplerApp', [
+        'freeSamplerApp.services'
+    ]);
+
+    /**
+     * @ngdoc function
+     * @name freeSamplerApp.controller:MainCtrl
+     * @description
+     * # MainCtrl
+     * Controller of the freeSamplerApp
+     */
+    samplerControllers.controller('MainCtrl', ['$scope', 'myHash',
+      function ($scope, myHash) {
+        $scope.blah = myHash('FOO') + 5;
+        $scope.awesomeThings = [
+          'HTML5 Boilerplate',
+          'AngularJS',
+          'Karma'
+        ];
+    }]);
+
+})();
