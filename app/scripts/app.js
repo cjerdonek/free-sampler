73b3c026c034e850738ad3a95409675bc24b7da8
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-08-09 18:00:17 -0700

    Run the Yeoman generator for AngularJS as follows:
        $ yo angular free-sampler
        Would you like to use Sass (with Compass)? No
        Would you like to include Bootstrap? Yes
        Which modules would you like to include?
        [no] angular-animate.js
        [no] angular-cookies.js
        [no] angular-resource.js
        [no] angular-route.js
        [no] angular-sanitize.js
        [no] angular-touch.js
    
    This was using version 0.9.5 of generator-angular from--
    https://github.com/yeoman/generator-angular

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name freeSamplerApp
+ * @description
+ * # freeSamplerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('freeSamplerApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name freeSamplerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the freeSamplerApp
+ */
+angular.module('freeSamplerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
