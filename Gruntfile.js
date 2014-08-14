f59d8bdd4a1892c68bc16ab56e679e3a70020da3
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-08-14 16:26:12 -0700

    Fix watch files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,6 +57,8 @@ module.exports = function (grunt) {
         },
         files: [
           '<%= yeoman.app %>/{,*/}*.html',
+          '<%= yeoman.app %>/templates/directives/{,*/}*.html',
+          '<%= yeoman.app %>/templates/views/{,*/}*.html',
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
