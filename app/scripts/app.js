d0d49c25e105804fee3653790b05bd1477fd5a94
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-08-14 09:56:42 -0700

    Load the directives script.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,20 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name freeSamplerApp
- * @description
- * # freeSamplerApp
- *
- * Main module of the application.
- */
-angular
-  .module('freeSamplerApp', []);
+/* Application */
+
+(function(){
+
+  /**
+   * @ngdoc overview
+   * @name freeSamplerApp
+   * @description
+   * # freeSamplerApp
+   *
+   * Main module of the application.
+   */
+  angular.module('freeSamplerApp', [
+      'freeSamplerApp.controllers',
+      'freeSamplerApp.directives'
+  ]);
+
+})();
