5a8041e8288bf6c32c19851d0325442b2d8efa53
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-08-17 11:43:49 -0700

    Start refactoring protractor conf to allow "profiles".

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,15 +1,25 @@
-exports.config = {
+
+// TODO: enclose this logic in an anonymous function.
+
+var profile = process.env.PROTRACTOR_PROFILE;
+console.log('protractor profile: ' + profile);
+
+var extra;
+if (profile === undefined) {
+    extra = {
+        capabilities: {
+            'browserName': 'chrome'
+        },
+        chromeOnly: true
+    }
+}
+
+var config = {
   allScriptsTimeout: 11000,
 
   specs: [
     'e2e/*.js'
   ],
-
-  capabilities: {
-    'browserName': 'chrome'
-  },
-
-  chromeOnly: true,
 
   baseUrl: 'http://localhost:9000/',
 
@@ -19,3 +29,9 @@ exports.config = {
     defaultTimeoutInterval: 30000
   }
 };
+
+for (var key in extra) {
+    config[key] = extra[key];
+}
+
+exports.config = config;
