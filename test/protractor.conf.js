c9d5c80c78375f4c37679f1360e7a552f7cb1d6e
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-08-17 12:02:53 -0700

    Use an anonymous function.

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,49 +1,51 @@
+'use strict';
 
-// TODO: enclose this logic in an anonymous function.
+exports.config = (function() {
 
-var profile = process.env.PROTRACTOR_PROFILE;
-console.log('protractor profile: ' + profile);
+    var profile = process.env.PROTRACTOR_PROFILE;
+    console.log('protractor profile: ' + profile);
 
-var extra;
-switch (profile) {
-    case undefined:
-        extra = {
-            capabilities: {
-                'browserName': 'chrome'
-            },
-            chromeOnly: true
-        };
-        break;
-    case 'phantomjs':
-        extra = {
-            capabilities: {
-               'browserName': 'phantomjs',
-               'phantomjs.binary.path': './node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin/phantomjs'
-            }
-        };
-        break;
-    default:
-        throw 'invalid protractor profile: ' + profile;
-}
+    var extra;
+    switch (profile) {
+        case undefined:
+            extra = {
+                capabilities: {
+                    'browserName': 'chrome'
+                },
+                chromeOnly: true
+            };
+            break;
+        case 'phantomjs':
+            extra = {
+                capabilities: {
+                   'browserName': 'phantomjs',
+                   'phantomjs.binary.path': './node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin/phantomjs'
+                }
+            };
+            break;
+        default:
+            throw 'invalid protractor profile: ' + profile;
+    }
 
-var config = {
-  allScriptsTimeout: 11000,
+    var config = {
+      allScriptsTimeout: 11000,
 
-  specs: [
-    'e2e/*.js'
-  ],
+      specs: [
+        'e2e/*.js'
+      ],
 
-  baseUrl: 'http://localhost:9000/',
+      baseUrl: 'http://localhost:9000/',
 
-  framework: 'jasmine',
+      framework: 'jasmine',
 
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
-  }
-};
+      jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+      }
+    };
 
-for (var key in extra) {
-    config[key] = extra[key];
-}
+    for (var key in extra) {
+        config[key] = extra[key];
+    }
 
-exports.config = config;
+    return config;
+}());
