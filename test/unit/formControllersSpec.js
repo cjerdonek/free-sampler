4f50f9097734bdb4437c591da11c380ae276f6e0
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-08-24 23:35:55 -0700

    Fix unit tests.

--- test/unit/formControllersSpec.js
+++ test/unit/formControllersSpec.js
@@ -15,33 +15,62 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  describe('Total Items field', function() {
-    var spellsInt;
+  // TODO: the tests below should not all need to call submit()?
+  describe('totalCount input', function() {
 
     beforeEach(function() {
-      scope.submit();
     });
 
     it('should display an error if empty', function() {
+      scope.form.submit();
       expect(scope.form.errors.totalCount).toEqual('A whole number bigger than zero is required.');
+    });
+
+    it('should clear the "sample count too large" error if the input changes', function() {
+      var form = scope.form;
+      var errors = form.errors;
+      var input = form.input;
+
+      input.totalCount = 100;
+      input.sampleCount = 10000;
+      form.submit();
+
+      expect(errors.sampleCount).not.toBeUndefined();
+      form.onInputChange('totalCount');
+      expect(errors.sampleCount).toBeUndefined();
+    });
+
+    it('should not clear the "invalid sample count" error if the input changes', function() {
+      var form = scope.form;
+      var errors = form.errors;
+      var input = form.input;
+
+      input.totalCount = 100;
+      input.sampleCount = 'abc';
+      form.submit();
+
+      expect(errors.sampleCount).not.toBeUndefined();
+      form.onInputChange('totalCount');
+      expect(errors.sampleCount).not.toBeUndefined();
     });
 
   });
 
-  describe('Sample Count field', function() {
-    var spellsInt;
+  describe('sampleCount input', function() {
 
     beforeEach(function() {
     });
 
     it('should display an error if sample count too large', function() {
-      var input = scope.form.input;
+      var form = scope.form;
+      var errors = form.errors;
+      var input = form.input;
 
       input.totalCount = 100;
       input.sampleCount = 1000;
-      scope.submit();
+      form.submit();
 
-      expect(scope.form.errors.sampleCount).toEqual('The sample count must be smaller than the total size.');
+      expect(errors.sampleCount).toEqual('The sample count must be smaller than the total count.');
     });
 
   });
